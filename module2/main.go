f42e3fbe75bd78976f43304bf1444d542531f966
xonnmy <32733071+xonnmy@users.noreply.github.com>
2022-10-09 12:05:05 +0800

    add

--- module2/main.go
+++ module2/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"io"
 	"net"
 	"net/http"
 	"os"
@@ -18,29 +18,40 @@ func httpAccessFunc(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set(k, v[0])
 		}
 	}
-	os.Setenv("VERSION", "V1.0")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "httpAccessFunc")
+}
 
-	//2. 获取环境变量"VERSION"
+//获取环境变量"VERSION"
+func getVersion(w http.ResponseWriter, r *http.Request) {
+	os.Setenv("VERSION", "V1.0")
 	name := os.Getenv("VERSION")
 	fmt.Println("VERSION Env: ", name)
+	io.WriteString(w, name)
+}
 
-	//打印ip
+//打印ip
+func getIP(w http.ResponseWriter, r *http.Request) {
+
 	ip, _, err := net.SplitHostPort(r.RemoteAddr)
 	if err != nil {
 		fmt.Println("获取ip错误:err,", err)
 	}
-	//判断ip
 	if net.ParseIP(ip) != nil {
-		fmt.Println("ip地址为:\n", ip)
+		fmt.Println("ip地址为:", ip)
 	}
-	fmt.Println("http 状态码:\n", http.StatusOK)
-	log.Println(http.StatusOK)
-
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "httpAccessFunc")
+	io.WriteString(w, ip)
+}
+func getCode(w http.ResponseWriter, r *http.Request) {
+	code := "200"
+	fmt.Println("code:", code)
+	w.Write([]byte(code))
 }
 func main() {
 	http.HandleFunc("/", httpAccessFunc)
 	http.HandleFunc("/healthzFunc", healthzFunc)
+	http.HandleFunc("/version", getVersion)
+	http.HandleFunc("/ip", getIP)
+	http.HandleFunc("/code", getCode)
 	http.ListenAndServe(":3000", nil)
 }
